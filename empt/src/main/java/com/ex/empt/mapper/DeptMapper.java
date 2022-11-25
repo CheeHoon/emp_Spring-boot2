baa65bd0b2a520917c249777828d5fcc36f15e8a
ch_Song <111099653+CheeHoon@users.noreply.github.com>
2022-11-25 15:01:17 +0900

    Add files via upload

---
+++ empt/src/main/java/com/ex/empt/mapper/DeptMapper.java
@@ -0,0 +1,8 @@
+package com.ex.empt.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DeptMapper {
+
+}
---
+++ empt/src/test/java/com/ex/empt/EmptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.empt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
